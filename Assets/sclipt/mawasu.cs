8488aa942ef13a952dda8952e8dbe7d9eb021f13
YusuKe010 <yuusuke.k1717@gmail.com>
2023-07-03 18:43:15 +0900

    スロットを止めたい

--- Assets/sclipt/mawasu.cs
+++ Assets/sclipt/mawasu.cs
@@ -1,32 +1,110 @@
 using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 /// <summary>
 /// Transform.RotateAround‚ğ—p‚¢‚½‰~‰^“®
 /// </summary>
 public class mawasu : MonoBehaviour
 {
-    // ’†S“_
+    /// <summary> ’†S“_/// </summary>
     [SerializeField] private Vector3 _center = Vector3.zero;
-    // ‰ñ“]²
+    /// <summary> ‰ñ“]²/// </summary>
     [SerializeField] private Vector3 _axis = Vector3.up;
     // ‰~‰^“®üŠú
     [SerializeField] private float _period = 2;
 
-    bool _rightRotate = false;
+    bool _Right = false;
+    bool _Center = false;
+    bool _Left = false;
+    GameObject _leftRotate = null;
+    GameObject _rightRotate = null;
+    [SerializeField] float[] list = { 0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, -180, -162, -144, -126, -108, -90, -72, -54, -36, -18 };
+
+    void Start()
+    {
+        
+
+
+    }
+
+
     private void Update()
     {
+        _leftRotate = GameObject.FindWithTag("left");
+        _rightRotate = GameObject.FindWithTag("right");
         // ’†S“_center‚Ìü‚è‚ğA²axis‚ÅAperiodüŠú‚Å‰~‰^“®
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S) && !_Right && !_Center && !_Left)
         {
-            _rightRotate = true;
+            _Left = true;
+            _Center = true;
+            _Right = true;
         }
-        if(_rightRotate )
+
+
+        if (_Left)
         {
-            transform.RotateAround(
-                _center,
-                _axis,
-                360 / _period * Time.deltaTime
-            );
+            _leftRotate.transform.RotateAround(_center, _axis, 360 / _period * Time.deltaTime);
         }
-        
+        if (_Center)
+        {
+            transform.RotateAround(_center, _axis, 360 / _period * Time.deltaTime);
+        }
+        if (_Right)
+        {
+            _rightRotate.transform.RotateAround(_center, _axis, 360 / _period * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _Left = false;
+            foreach (var RotationLeft in list)
+            {
+
+                if (_leftRotate.transform.rotation.x >= RotationLeft - 9f && _leftRotate.transform.rotation.x < RotationLeft + 9f)
+                {
+                    Debug.Log(RotationLeft);
+                    _leftRotate.transform.RotateAround(_center, _axis, RotationLeft);
+                    break;
+                }
+
+
+
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            _Center = false;
+            foreach (var RotationCenter in list)
+            {
+                if (transform.rotation.x >= RotationCenter - 9 && transform.rotation.x < RotationCenter + 9)
+                {
+                    Debug.Log(RotationCenter);
+                    transform.RotateAround(_center, _axis, RotationCenter);
+                    break;
+                }
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            _Right = false;
+            foreach (var RotationRight in list)
+            {
+
+                if (_rightRotate.transform.rotation.x >= RotationRight - 9 && _rightRotate.transform.rotation.x < RotationRight + 9)
+                {
+                    Debug.Log(RotationRight);
+                    _rightRotate.transform.RotateAround(_center, _axis, RotationRight);
+                    break;
+                }
+            }
+
+        }
+
+
+
     }
 }
