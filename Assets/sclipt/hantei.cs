3b940609a5b2c9a687604ea18fade795adcd9629
YusuKe010 <yuusuke.k1717@gmail.com>
2023-07-06 12:02:23 +0900

    横1列に止めることに成功

--- Assets/sclipt/hantei.cs
+++ Assets/sclipt/hantei.cs
@@ -13,6 +13,11 @@ public class hantei : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+          
     }
 }
--- Assets/sclipt/mawasu.cs
+++ Assets/sclipt/mawasu.cs
@@ -1,110 +1,120 @@
-using UnityEngine;
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-/// <summary>
-/// Transform.RotateAroundを用いた円運動
-/// </summary>
+using UnityEngine;
+
 public class mawasu : MonoBehaviour
 {
-    /// <summary> 中心点/// </summary>
-    [SerializeField] private Vector3 _center = Vector3.zero;
-    /// <summary> 回転軸/// </summary>
-    [SerializeField] private Vector3 _axis = Vector3.up;
-    // 円運動周期
-    [SerializeField] private float _period = 2;
 
-    bool _Right = false;
-    bool _Center = false;
-    bool _Left = false;
-    GameObject _leftRotate = null;
-    GameObject _rightRotate = null;
-    [SerializeField] float[] list = { 0, 18, 36, 54, 72, 90, 108, 126, 144, 162, 180, -180, -162, -144, -126, -108, -90, -72, -54, -36, -18 };
+    [SerializeField] GameObject reel1;
+    [SerializeField] GameObject reel2;
+    [SerializeField] GameObject reel3;
+    BoxCollider2D _colliderUp;
+    BoxCollider2D _colliderCenter;
+    BoxCollider2D _colliderDown;
+    [SerializeField] GameObject hanteiUp;
+    [SerializeField] GameObject hanteiCenter;
+    [SerializeField] GameObject hanteiUnder;
 
+    Vector3 initialpos;
+    Vector3 initialpos2;
+    Vector3 initialpos3;
+
+    float speed1;
+    float speed2;
+    static float speed3;
+
+    bool stopflag1 = false;//ボタンが押されたかどうか
+    bool stopflag2 = false;
+    bool stopflag3 = false;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        
-
-
+        initialpos = reel1.transform.position;
+        initialpos2 = reel2.transform.position;
+        initialpos3 = reel3.transform.position;
+        _colliderUp = hanteiUp.GetComponent<BoxCollider2D>();
+        _colliderCenter = hanteiCenter.GetComponent<BoxCollider2D>();
+        _colliderDown = hanteiUnder.GetComponent<BoxCollider2D>();
     }
 
-
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        _leftRotate = GameObject.FindWithTag("left");
-        _rightRotate = GameObject.FindWithTag("right");
-        // 中心点centerの周りを、軸axisで、period周期で円運動
-        if (Input.GetKeyDown(KeyCode.S) && !_Right && !_Center && !_Left)
+            this.reel1.transform.Translate(0, speed1, 0);
+            this.reel2.transform.Translate(0, speed2, 0);
+            this.reel3.transform.Translate(0, speed3, 0);
+        
+        if (reel1.transform.position.y < -50f)
         {
-            _Left = true;
-            _Center = true;
-            _Right = true;
+            Debug.Log("aaa");
+            reel1.transform.position = initialpos;
+        }
+        if (reel2.transform.position.y < -50f)
+        {
+            reel2.transform.position = initialpos2;
+        }
+        if (reel3.transform.position.y < -50f)
+        {
+            reel3.transform.position = initialpos3;
         }
 
+        if (stopflag1 && 0f <= Mathf.Abs(reel1.transform.position.y % 2f) && Mathf.Abs(reel1.transform.position.y % 2f) < 0.2f)
+        {
+            speed1 = 0;
+            _colliderUp.enabled = true;
+        }
+        if (stopflag2 && 0f <= Mathf.Abs(reel2.transform.position.y % 2f) && Mathf.Abs(reel2.transform.position.y % 2f) < 0.2f)
+        {
+            speed2 = 0;
+            _colliderCenter.enabled = true;
+        }
+        if (stopflag3 && 0f <= Mathf.Abs(reel3.transform.position.y % 2f) && Mathf.Abs(reel3.transform.position.y % 2f) < 0.2f)
+        {
+            speed3 = 0;
+            _colliderDown.enabled = true;
+        }
 
-        if (_Left)
+        if (Input.GetKeyDown(KeyCode.S)&& speed1 == 0 && speed2 == 0 && speed3 == 0)
         {
-            _leftRotate.transform.RotateAround(_center, _axis, 360 / _period * Time.deltaTime);
-        }
-        if (_Center)
-        {
-            transform.RotateAround(_center, _axis, 360 / _period * Time.deltaTime);
-        }
-        if (_Right)
-        {
-            _rightRotate.transform.RotateAround(_center, _axis, 360 / _period * Time.deltaTime);
-        }
+            stopflag1 = false;
+            stopflag2 = false;
+            stopflag3 = false;
 
-        if (Input.GetKeyDown(KeyCode.D))
+            speed1 = -0.2f;
+            speed2 = -0.2f;
+            speed3 = -0.2f;
+
+            _colliderUp.enabled = false;
+            _colliderCenter.enabled=false;
+            _colliderDown.enabled = false;
+        }
+        
+
+        if(Input.GetKeyDown(KeyCode.F))
         {
-            _Left = false;
-            foreach (var RotationLeft in list)
+            if (stopflag1 != true)//ボタンがまだ押されてない場合
             {
-
-                if (_leftRotate.transform.rotation.x >= RotationLeft - 9f && _leftRotate.transform.rotation.x < RotationLeft + 9f)
-                {
-                    Debug.Log(RotationLeft);
-                    _leftRotate.transform.RotateAround(_center, _axis, RotationLeft);
-                    break;
-                }
-
-
-
+                speed1 = -0.03f;//リールをゆっくりにする
             }
-
+            stopflag1 = true;
         }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            _Center = false;
-            foreach (var RotationCenter in list)
-            {
-                if (transform.rotation.x >= RotationCenter - 9 && transform.rotation.x < RotationCenter + 9)
-                {
-                    Debug.Log(RotationCenter);
-                    transform.RotateAround(_center, _axis, RotationCenter);
-                    break;
-                }
-            }
-        }
-
-
         if (Input.GetKeyDown(KeyCode.G))
         {
-            _Right = false;
-            foreach (var RotationRight in list)
+            if (stopflag2 != true)//ボタンがまだ押されてない場合
             {
-
-                if (_rightRotate.transform.rotation.x >= RotationRight - 9 && _rightRotate.transform.rotation.x < RotationRight + 9)
-                {
-                    Debug.Log(RotationRight);
-                    _rightRotate.transform.RotateAround(_center, _axis, RotationRight);
-                    break;
-                }
+                speed2 = -0.03f;//リールをゆっくりにする
             }
-
+            stopflag2 = true;
         }
-
-
-
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (stopflag3 != true)//ボタンがまだ押されてない場合
+            {
+                speed3 = -0.03f;//リールをゆっくりにする
+            }
+            stopflag3 = true;
+        }
+        
     }
 }
